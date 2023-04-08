7f9c8004e295538b48356ed7fdf6964d16d9f29d
Timilehin Okunola <okunolatimilehin4123@gmail.com>
2023-04-08 15:21:53 +0100

    updated changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { UserController } from './user/user.controller';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [GraphQLModule],
+  imports: [GraphQLModule, AuthModule],
+  controllers: [UserController],
   // controllers: [AppController],
   // providers: [AppService],
 })
