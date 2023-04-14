76cbfd18559899101b4db440b81d1482d4f6bd7d
Timilehin Okunola <okunolatimilehin4123@gmail.com>
2023-04-14 23:40:15 +0100

    Created Lesson folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,25 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
-import { UserController } from './user/user.controller';
-import { AuthModule } from './auth/auth.module';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { LessonModule } from './lesson/lesson.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Lesson } from './lesson/lesson.entity';
 
 @Module({
-  imports: [GraphQLModule, AuthModule],
-  controllers: [UserController],
-  // controllers: [AppController],
-  // providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: 'mongodb://localhost/school',
+      synchronize: true,
+      useUnifiedTopology: true,
+      entities: [Lesson],
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+    }),
+    LessonModule,
+  ],
+  controllers: [],
 })
 export class AppModule {}
